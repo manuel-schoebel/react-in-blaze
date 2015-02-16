95d21d524b2fe7bb80f7cdf7bd601c65ac459ff0
Avital Oliver <avital@thewe.net>
2015-02-16 15:18:51 -0800

    initial

---
+++ package.js
@@ -0,0 +1,27 @@
+Package.describe({
+  name: 'react-in-blaze',
+  version: '0.0.1',
+  // Brief, one-line summary of the package.
+  summary: 'Use React components in Blaze template',
+  // URL to the Git repository containing the source code for this package.
+  git: '',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.0.3.1');
+  api.use('templating');
+  api.addFiles(['react-component.html', 'react-component.js'], 'client');
+});
+
+/*
+ * TODO: add tests
+ *
+Package.onTest(function(api) {
+  api.use('tinytest');
+  api.use('react-in-blaze');
+  api.addFiles('react-in-blaze-tests.js');
+});
+ */
