ff4431594ce4c0111a9139f9ba887da5d39c464f
Avital Oliver <avital@thewe.net>
2015-02-16 15:21:14 -0800

    Update name and add README

---
+++ README.md
@@ -0,0 +1,7 @@
+Let's you easily use React components within Blaze templates, via:
+
+```html
+{{> ReactComponent type="Thing" props=.}}
+```
+
+TODO: A lot. :)
--- package.js
+++ package.js
@@ -1,5 +1,5 @@
 Package.describe({
-  name: 'react-in-blaze',
+  name: 'avital:react-in-blaze',
   version: '0.0.1',
   // Brief, one-line summary of the package.
   summary: 'Use React components in Blaze template',
